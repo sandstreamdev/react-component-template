a5872226fee1194350ceb77d8c4cc65eb8415d17
Marek Rozmus <marek.rozmus@sandstream.pl>
2019-12-05 10:42:08 +0100

    Fix code review issues

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,11 @@
 module.exports = {
-  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['src/**/*.js'],
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // A map from regular expressions to module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy'
   },
-
-  // The glob patterns Jest uses to detect test files
   testMatch: ['**/*.test.js'],
-
-  // A map from regular expressions to paths to transformers
   transform: {
     '\\.js$': ['babel-jest'],
     '.+\\.(css)$': 'jest-transform-css'
